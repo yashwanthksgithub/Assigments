259a536673a475adbb7fb157d6f1f909b18e9f93
yashwanthksgithub <103103937+yashwanthksgithub@users.noreply.github.com>
2022-04-07 14:53:54 +0530

    Create positive_no.py
    prints the range of the given positive numbers

---
+++ positive_no.py
@@ -0,0 +1,8 @@
+start=int(input("Enter the starting number :\n"))
+end=int(input("Enter the ending number :\n"))
+
+print("Positive numbers are :")
+for i in range(start,end+1):
+    print(i)
+
+print("Done")
