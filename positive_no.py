1ae9a8a1165c23310a10856ec7e201832749a1e9
yashwanthksgithub <103103937+yashwanthksgithub@users.noreply.github.com>
2022-04-07 15:48:54 +0530

    Update positive_no.py
    PRINTS:
    1)the given range of numbers
    2)positive numbers of the given list
    3)prints the positive numbers of the list in the form of list

--- positive_no.py
+++ positive_no.py
@@ -1,8 +1,24 @@
-start=int(input("Enter the starting number :\n"))
+#TO LIST THE RANGE OF POSITIVE NUMBERS
+'''start=int(input("Enter the starting number :\n"))
 end=int(input("Enter the ending number :\n"))
 
 print("Positive numbers are :")
 for i in range(start,end+1):
     print(i)
 
-print("Done")
+print("Done")'''
+
+list1 = [12, -7, 5, 64, -14]
+
+#TO PRINT THE NUMBERS : 
+
+for num in list1:
+    if num >= 0:
+       print(num, end = ",")
+
+
+#TO PRINT AS LIST:
+
+print("\nthe number in the list\n")
+list2= [i for i in list1 if i>= 0] 
+print(list2)
