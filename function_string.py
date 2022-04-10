25b3c28a319141a2b2b12214599fc053636ad2f6
yashwanthksgithub <103103937+yashwanthksgithub@users.noreply.github.com>
2022-04-10 10:31:52 +0530

    Update function_string.py

--- function_string.py
+++ function_string.py
@@ -1,7 +1,7 @@
 w= input('enter a string :\n')
 d = dict()
 
-def count(string):
+def most_frequent(string):
 #    d = dict()
     for key in string:
         if key not in d:
@@ -10,7 +10,7 @@ def count(string):
             d[key] += 1      
     return d
 
-dic2=dict(sorted(count(w).items(),key= lambda x:x[1]))
+dic2=dict(sorted(most_frequent(w).items(),key= lambda x:x[1]))
 
 res =dict(reversed(list(dic2.items())))
 print(res)
